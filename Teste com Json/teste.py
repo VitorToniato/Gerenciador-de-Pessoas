00583fe73f5fa73127af494e1475c54dd890ee64
VitorToniato <vitortoniato12@gmail.com>
2025-07-10 16:29:56 -0300

    Processando...

--- Teste com Json/teste.py
+++ Teste com Json/teste.py
@@ -7,14 +7,20 @@ repetir = int(input('Digite quantos nomes deseja inserir: '))
 
 for i in range(1, repetir + 1):
     nome = input('Digite o nome que deseja inserir: ')
+    idade = input('Digite sua idade')
 
     dic.append({
-        'Nome': nome
+        'Nome': nome, 
+        'Idade': idade
         
         })
     
+json_data = json.dumps(dic)
 
 with open('Teste com Json/pessoas.json', 'w') as arquivos:
-    json.dump(dic, arquivos)
+    json.dump(dic, arquivos, indent=4)
 
+
+
+print(json_data)
 print()
